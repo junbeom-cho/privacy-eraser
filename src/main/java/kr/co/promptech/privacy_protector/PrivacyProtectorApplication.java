049ee54e58b8d6af7f85660566a9db203a6c4f5d
jb cho <jb.cho@promptech.co.kr>
2026-08-07 16:23:26 +0900

    Initial Commit

---
+++ src/main/java/kr/co/promptech/privacy_protector/PrivacyProtectorApplication.java
@@ -0,0 +1,13 @@
+package kr.co.promptech.privacy_protector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivacyProtectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivacyProtectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/promptech/privacy_protector/PrivacyProtectorApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.promptech.privacy_protector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivacyProtectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
